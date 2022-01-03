a90c208b0126e1ee12bd8f8223c1757646d801d1
Emgariko <emil2001garipov@gmail.com>
2022-01-03 20:57:29 +0000

    1.2

--- hw3/src/HW3/Base.hs
+++ hw3/src/HW3/Base.hs
@@ -1,22 +1,32 @@
 module HW3.Base (
-    HiFun,
-    HiValue,
-    HiExpr,
+    HiFun (..),
+    HiValue (..),
+    HiExpr (..),
     HiError
     ) where
 
+-- FIXME: deriving Show
+
+-- function names (e.g. div, sort, length, ...)
 data HiFun = HiFunDiv
   | HiFunMul
   | HiFunAdd
-  | HiFunSub    -- function names (e.g. div, sort, length, ...)
+  | HiFunSub
+  deriving Show
 
+-- values (numbers, booleans, strings, ...)
 data HiValue = HiValueNumber Rational
-  | HiValueFunction HiFun -- values (numbers, booleans, strings, ...)
+  | HiValueFunction HiFun
+  deriving Show
 
+-- expressions (literals, function calls, ...)
 data HiExpr = HiExprValue HiValue
-  | HiExprApply HiExpr [HiExpr] -- expressions (literals, function calls, ...)
+  | HiExprApply HiExpr [HiExpr]
+  deriving Show
+
+-- evaluation errors (invalid arguments, ...)
 data HiError = HiErrorInvalidArgument
   | HiErrorInvalidFunction
   | HiErrorArityMismatch
   | HiErrorDivideByZero
-    deriving Show -- evaluation errors (invalid arguments, ...)
+  deriving Show
--- hw3/src/HW3/Parser.hs
+++ hw3/src/HW3/Parser.hs
@@ -1,23 +1,61 @@
 module HW3.Parser (
-    parse
+    parse, pHiFun, pHiValue, pHiExpr
     ) where
 
-import HW3.Base (HiExpr, HiError)
+import HW3.Base (HiExpr (..), HiFun (..), HiValue (..))
 import Data.Void (Void)
 import Text.Megaparsec.Error ( ParseErrorBundle )
-import Text.Megaparsec (Parsec, satisfy, runParser, (<|>))
-import Text.Megaparsec.Byte (string)
+import Text.Megaparsec (Parsec, (<|>), parseTest, choice, many, runParser)
+import Text.Megaparsec.Char.Lexer (scientific)
+import qualified Data.Scientific
+import Text.Megaparsec.Char (char, space, string)
 
 parse :: String -> Either (ParseErrorBundle String Void) HiExpr
-parse = undefined
+parse = runParser pHiExpr ""
 
-type Parser = Parsec HiError String
+type Parser = Parsec Void String
 
-pScheme :: Parser String
-pScheme = string "data"
-  <|> string "file"
-  <|> string "ftp"
-  <|> string "http"
-  <|> string "https"
-  <|> string "irc"
-  <|> string "mailto"
+pHiFun :: Parser HiFun
+pHiFun = choice
+  [ HiFunDiv <$ string "div"
+  , HiFunMul <$ string "mul"
+  , HiFunAdd <$ string "add"
+  , HiFunSub <$ string "sub"]
+
+pHiValueNumber :: Parser Data.Scientific.Scientific
+pHiValueNumber = scientific
+
+pHiValue :: Parser HiValue
+pHiValue = choice
+  [ HiValueFunction <$> pHiFun,
+    HiValueNumber . toRational <$> pHiValueNumber]
+
+pHiExprArgs :: Parser [HiExpr]
+pHiExprArgs = do
+  arg <- pHiExpr
+  rest <- many (char ',' *> space *> pHiExpr)   -- FIXME: where space should be consumed?
+  return (arg : rest)
+
+-- Args   -> HiExpr ArgEnd
+-- ArgEnd -> , HiExpr ArgEnd
+-- ArgEnd -> eps
+
+pHiExpr_ :: HiExpr -> Parser HiExpr
+pHiExpr_ expr = do
+  _ <- char '('
+  exprArgs <- pHiExprArgs <|> pure [] -- FIXME: ?
+  _ <- char ')'
+  let expr_ = HiExprApply expr exprArgs
+  pHiExpr_  expr_ <|> pure expr_
+
+pHiExprValue :: Parser HiExpr
+pHiExprValue = HiExprValue <$> pHiValue
+
+pHiExpr :: Parser HiExpr
+pHiExpr = do
+  v <- pHiExprValue
+  pHiExpr_ v <|> pure v -- FIXME: ?
+
+-- HiExpr   -> hv HiExpr'
+-- HiExpr'  -> (Args) HiExpr'
+-- HiExpr'  -> eps
