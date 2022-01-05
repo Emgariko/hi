b8886b7bf85590b6cc530593dd45581e9e256811
Emgariko <emil2001garipov@gmail.com>
2022-01-05 12:04:14 +0000

    eval

--- hw3/src/HW3/Evaluator.hs
+++ hw3/src/HW3/Evaluator.hs
@@ -4,11 +4,17 @@ module HW3.Evaluator (
 
 import HW3.Base (HiExpr (..), HiError (..), HiValue (..), HiFun (HiFunDiv, HiFunAdd, HiFunSub, HiFunMul))
 import Data.Functor.Identity (Identity)
-import Control.Monad.Except (ExceptT, MonadError (throwError))
+import Control.Monad.Except (ExceptT, MonadError (throwError), runExceptT, runExcept)
 import Data.Functor.Classes (eq1)
 
 -- type ExceptTm m = ExceptT HiError m HiValue
-type ExceptTm = ExceptT HiError Identity HiValue
+type ExceptTm m = ExceptT HiError m HiValue
+
+mapToOperation :: HiFun -> (Rational -> Rational -> Rational)
+mapToOperation HiFunAdd = (+)
+mapToOperation HiFunSub = (-)
+mapToOperation HiFunMul = (*)
+mapToOperation HiFunDiv = (/)
 
 getArity :: HiFun -> Int
 getArity HiFunDiv = 2
@@ -27,25 +33,23 @@ validateArgs :: HiFun -> HiValue -> HiValue -> Maybe HiError
 validateArgs HiFunDiv (HiValueNumber v1) (HiValueNumber v2) = case v2 of
                                                                 0 -> Just HiErrorDivideByZero
                                                                 _ -> Nothing
+validateArgs _ _ _ = Nothing
 
-twoArgFun :: HiExpr -> HiExpr -> (Rational -> Rational -> Rational) -> ExceptTm
-twoArgFun e1 e2 f = do
+twoArgFun :: Monad m => HiFun -> HiExpr -> HiExpr -> (Rational -> Rational -> Rational) -> ExceptTm m
+twoArgFun fun e1 e2 f = do
     v1 <- evalHiExpr e1
     v2 <- evalHiExpr e2
-    return $ evalHiFun_ v1 v2 f
+    let ok = validateArgs fun v1 v2
+    case ok of
+        Nothing -> return $ evalHiFun_ v1 v2 f
+        Just err -> throwError err
 
-evalHiFun :: HiFun -> [HiExpr] -> ExceptTm
-evalHiFun HiFunAdd [e1, e2] = twoArgFun e1 e2 (+)
-
-evalHiFun HiFunSub [e1, e2] = twoArgFun e1 e2 (-)
-
-evalHiFun HiFunMul [e1, e2] = twoArgFun e1 e2 (*)
-
-evalHiFun HiFunDiv [e1, e2] = twoArgFun e1 e2 (/)
+evalHiFun :: Monad m => HiFun -> [HiExpr] -> ExceptTm m
+evalHiFun fun [e1, e2] = twoArgFun fun e1 e2 $ mapToOperation fun
 
 evalHiFun _ _ = error "unexpected function call"
 
-checkArityAndThenEval :: HiFun -> [HiExpr] -> ExceptTm
+checkArityAndThenEval :: Monad m => HiFun -> [HiExpr] -> ExceptTm m
 checkArityAndThenEval fun args =
     let ok = checkArity fun args in
         if ok
@@ -53,15 +57,15 @@ checkArityAndThenEval fun args =
             else throwError HiErrorArityMismatch
 
 
-evalHiExprApply :: HiExpr -> [HiExpr] -> ExceptTm
+evalHiExprApply :: Monad m => HiExpr -> [HiExpr] -> ExceptTm m
 evalHiExprApply (HiExprValue (HiValueFunction hiFun)) args = checkArityAndThenEval hiFun args
 evalHiExprApply _ _ = throwError HiErrorInvalidFunction
 
-evalHiExpr :: HiExpr -> ExceptTm
+evalHiExpr :: Monad m => HiExpr -> ExceptTm m
 evalHiExpr (HiExprValue a@(HiValueNumber _)) = return a
 evalHiExpr (HiExprValue (HiValueFunction _)) = throwError HiErrorInvalidArgument
 evalHiExpr (HiExprApply expr args) = evalHiExprApply expr args
 
 eval :: Monad m => HiExpr -> m (Either HiError HiValue)
 -- eval expr = evalHiExpr expr
-eval = undefined
+eval expr = runExceptT (evalHiExpr expr)
